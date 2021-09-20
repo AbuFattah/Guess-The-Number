dbe226cb2bc44ad9654dddfe5c7aea4caac6a4ad
AbuFattah <mdnahid142493@gmail.com>
2021-09-21 00:34:25 +0600

    First commit

---
+++ app.js
@@ -0,0 +1,98 @@
+/* 
+  GAME RULES:
+  The user is required to guess a number between min and max.
+  If user guess match with the winning number --> show win msg.
+  The player will have a certain amout of guesses.
+  Show remaining guesses to player.
+  If guess left becomes ZERO, show lose msg with winning number.
+  Let the player choose to play again after the game is over.
+*/
+// Game values
+let minVal = 1,
+    maxVal = 10,
+    winningNum = getRandomInt(1,10),
+    guessesLeft = 3,
+    blue = '#11b4f5';
+
+//UI Elements
+const gameForm = document.querySelector('#game-form'),
+      minNum = document.querySelector('.min-num'),
+      maxNum = document.querySelector('.max-num'),
+      guessInput = document.querySelector('#guess-input'),
+      guessBtn = document.querySelector('#guess-btn'),
+      msg = document.querySelector('#msg');
+
+
+
+// Adding ui min and max
+minNum.textContent = minVal;
+maxNum.textContent = maxVal;
+
+//adding event listeners
+
+gameForm.addEventListener('submit',function(e){
+  let guess = parseInt(guessInput.value);
+  
+  //validation
+  if(isNaN(guess) || guess<minVal || guess > maxVal){
+    
+    setMsg(`Please enter a number between ${minVal} and ${maxVal}`,'red' );
+  }
+  else{
+      //Win case
+  if(guess === winningNum){
+    guessInput.disabled = true;
+    /* guessInput.borderColor = 'green'; */
+    guessInput.classList.add('border-success');
+    guessBtn.classList.add('border-success');
+    setMsg(`YOU WIN, ${winningNum} is correct!`,'green');
+    // changing value to Play again
+    guessBtn.value = 'Play Again'
+    // reload function after game over
+    guessBtn.addEventListener('click',function(){
+      window.location.reload('Refresh');
+    });
+  }
+  else{
+    guessesLeft -=1;
+    console.log(guessesLeft);
+    setMsg(`You have ${guessesLeft} guesses left.`,blue);
+
+    if(guessesLeft === 0){
+      guessInput.disabled = true;
+      guessInput.classList.add('border-danger');
+      guessBtn.classList.add('border-danger');
+      setMsg(` It was ${winningNum}, You lost!!`,'red');
+
+      guessBtn.value = 'Play Again'
+      guessBtn.addEventListener('click',function(){
+        window.location.reload('Refresh');
+      });
+    }
+  }
+  }
+
+  e.preventDefault();
+});
+
+
+function setMsg(message,color){
+  msg.textContent = message;
+  msg.style.color = color;
+}
+
+// Random Integer generator function
+
+function getRandomInt(min,max){
+  return Math.floor(Math.random()*(max-min+1)) + min;
+}
+
+
+
+
+
+
+
+
+
+
